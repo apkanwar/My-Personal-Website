bf4d8908ee190212a45a57563331ca619b44904a
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2017-05-09 09:34:41 -0400

    changed number to phone

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,17 +1,17 @@
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $number = $_POST['number']
+    $phone = $_POST['number']
     $message = $_POST['message'];
     $from = 'From: APK'; 
     $to = 'ap_kanwar@outlook.com'; 
     $subject = 'Personal Website Message';
     $human = $_POST['human'];
          
-    $body = "From: $name\n E-Mail: $email\n Phone Number: $number\n Message:\n $message";
+    $body = "From: $name\n E-Mail: $email\n Phone Number: $phone\n Message:\n $message";
             
     if ($_POST['submit'] && $human == '4') {           
-        if (mail ($to, $subject, $body, $from, $number)) { 
+        if (mail ($to, $subject, $body, $from, $phone)) { 
        echo '<p>Your message has been sent!</p>';
    } else { 
        echo '<p>Something went wrong, go back and try again!</p>'; 
