1298521685e32c53e1e9c42f5b97b7094b4443ca
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2017-05-12 11:27:03 -0400

    Testing contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,22 +1,41 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['number']
-    $message = $_POST['message'];
-    $from = 'From: APK';
-    $to = 'ap_kanwar@outlook.com';
-    $subject = 'Personal Website Message';
-    $human = $_POST['human'];
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'Demo Contact Form';
+		$to = 'ap_kanwar@outlook.com';
+		$subject = 'Message from Contact Demo ';
 
-    $body = "From: $name\n E-Mail: $email\n Phone Number: $phone\n Message:\n $message";
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit'] && $human == '4') {
-        if (mail ($to, $subject, $body, $from, $phone)) {
-       echo '<p>Your message has been sent!</p>';
-   } else {
-       echo '<p>Something went wrong, go back and try again!</p>';
-   }
-    } else if ($_POST['submit'] && $human != '4') {
-   echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
 ?>
